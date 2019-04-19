904b05e32dbc4fac443feb088f9bb86ad9a4c542
Nik7ggg <47344019+Nik7ggg@users.noreply.github.com>
2019-04-19 09:57:16 +0700

    Норм рабит

--- TecProg_OOPVersion/main.cpp
+++ TecProg_OOPVersion/main.cpp
@@ -20,7 +20,7 @@ int main(int argc, char* argv[])
 	container c;
 	c.In(ifst);
 	ofst << "Filled container. " << endl;
-	c.Out_Truck(ofst);
+	c.Out(ofst);
 	c.~container();//очистка контейнера
 	ofst << "Empty container. " << endl;
 	c.Out(ofst);
